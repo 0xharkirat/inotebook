61168290d249590f37ffa72d5628f6f9476467e7
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2022-11-29 19:11:45 +1100

    49

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,11 @@ const express = require("express");
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Harryisagoodboy';
+
 
 router.post(
   "/createuser",
@@ -28,13 +33,26 @@ router.post(
           if(user){
               return res.status(400).json({error: "Sorry a user with this email already exists"})
           }
+
+          const salt = await bcrypt.genSalt(10);
+          const secPass = await bcrypt.hash(req.body.password, salt);
+        //   Create a new user
           user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: secPass,
           })
+
+          const data = {
+            user: {
+                id: user.id
+            }
+          }
+
+          const authtoken = jwt.sign(data, JWT_SECRET);
       
-          res.json(user);
+        //   res.json(user);
+          res.json(authtoken);
     } 
     
     catch (error) {
