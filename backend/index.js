a9c2f8f7145c9f5eaee6768bdfc98ada59297957
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2022-11-29 17:18:58 +1100

    45

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,26 @@
-const connectToMongo = require('./db');
+//jshint esversion:6
 
+const connectToMongo = require("./db");
+const express = require("express");
+
+// const bodyParser = require('body-parser');
 
 
 connectToMongo();
 
-//jshint esversion:6
-const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', function(req, res){
-res.send('Hello Harry')
+app.use(express.json())
+// app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/notes", require("./routes/notes"));
+
+
+app.get("/", function (req, res) {
+  res.send("Hello Harry");
 });
 
-app.listen(port, function(){
-console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, function () {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
