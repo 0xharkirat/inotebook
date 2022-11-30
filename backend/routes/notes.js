15e458b7e1cc593b771af85891d02170f359ad24
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2022-11-30 19:38:34 +1100

    53

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -48,4 +48,31 @@ router.post(
     }
 );
 
+// ROUTE 3: Update an existing note using "PUT"
+router.put("/updatenote/:id", fetchuser, async (req, res)=>{
+    const {title, description, tag} = req.body;
+
+    // Create a newNote object
+    const newNote = {};
+
+    if (title){newNote.title = title};
+    if(description){newNote.description= description};
+    if(tag){newNote.title = title};
+
+    // Find the note to be updated and update it
+    let note = await Notes.findById(req.params.id);
+    if(!note){return res.statusMessage(404).send("Not Found")}
+
+    // console.log(note);
+    // console.log(req.user);
+    
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+
+    note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+    res.json({note})
+})
+
 module.exports = router;
