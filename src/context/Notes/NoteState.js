93cc090b4ef32eecb609f80a4624b2ff873aa6d1
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2022-12-02 12:41:32 +1100

    62

--- src/context/Notes/NoteState.js
+++ src/context/Notes/NoteState.js
@@ -5,7 +5,7 @@ import noteContext from "./noteContext"
 const NoteState = (props)=>{
    const notesInitial = [
     {
-      "_id": "6387067371406c0cd7e40b12",
+      "_id": "638706737140a6c0cd7e40b12",
       "user": "6385bdbc9706441e605c6cdb",
       "title": "My title",
       "description": "Please wake up early",
@@ -14,7 +14,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "6387067471406c0cd7e40b14",
+      "_id": "63870674714d06c0cd7e40b14",
       "user": "6385bdbc9706441e605c6cdb",
       "title": "My title",
       "description": "Please wake up early",
@@ -23,7 +23,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "6387067471406c0cd7e40b16",
+      "_id": "6387067471c406c0cd7e40b16",
       "user": "6385bdbc9706441e605c6cdb",
       "title": "My title",
       "description": "Please wake up early",
@@ -32,7 +32,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "6387067471406c0cd7e40b14",
+      "_id": "63870674714v06c0cd7e40b14",
       "user": "6385bdbc9706441e605c6cdb",
       "title": "My title",
       "description": "Please wake up early",
@@ -41,7 +41,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "6387067471406c0cd7e40b16",
+      "_id": "63870674714h06c0cd7e40b16",
       "user": "6385bdbc9706441e605c6cdb",
       "title": "My title",
       "description": "Please wake up early",
@@ -50,7 +50,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "6387067471406c0cd7e40b14",
+      "_id": "638706747140j6c0cd7e40b14",
       "user": "6385bdbc9706441e605c6cdb",
       "title": "My title",
       "description": "Please wake up early",
@@ -59,7 +59,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "6387067471406c0cd7e40b16",
+      "_id": "6387067471406ck0cd7e40b16",
       "user": "6385bdbc9706441e605c6cdb",
       "title": "My title",
       "description": "Please wake up early",
@@ -68,7 +68,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "6387067471406c0cd7e40b14",
+      "_id": "6387067471406cl0cd7e40b14",
       "user": "6385bdbc9706441e605c6cdb",
       "title": "My title",
       "description": "Please wake up early",
@@ -77,7 +77,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "6387067471406c0cd7e40b16",
+      "_id": "6387067471406c;0cd7e40b16",
       "user": "6385bdbc9706441e605c6cdb",
       "title": "My title",
       "description": "Please wake up early",
